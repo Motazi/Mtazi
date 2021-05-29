1edaae7c33ae72200897aa5a72fabe4a3a004fb8
Motazi <72793033+Motazi@users.noreply.github.com>
2021-05-29 18:45:28 +0100

    Add files via upload

--- Prompt.c
+++ Prompt.c
@@ -2,22 +2,30 @@
 
 void prompt(char command[])
 {
-
   while(true)
   {
-
+    int a = 0;
     printf(">");
     fgets(command, 100,stdin);
-    printf("%s\n",command);
-
+    command[strlen(command) - 1] = 0;
+    a = strcmp(command, "exit");
+    if (a==0)
+    {
+      exit(1);
+    }
   }
 
 }
 
 int main(int argc, char* argv[])
 {
+
   char command[100] = "";
   prompt(command);
 
+
+
+
 }
+
 
--- Prompt.h
+++ Prompt.h
@@ -6,11 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-  char* buffer;
-  size_t buffer_length;
-  size_t input_length;
-} InputBuffer;
 void prompt();
+
+
 
 #endif
